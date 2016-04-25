adca4eb541ba60e03e3a0cc4723cb7724323d151
Samuel Sanchez <sjs83197@gmail.com>
2016-04-25 15:25:27 -0700

    Header add to googleMapsMapping.py

---
+++ googleMapsMapping.py
@@ -0,0 +1,6 @@
+"""
+Author: Samuel Sanchez
+GitHub: SamuelSanchezA
+Description: US chronic diseases reported by the CDC between 2007-2013 
+			 are mapped onto a map of the United States (continental and offshore)
+"""
